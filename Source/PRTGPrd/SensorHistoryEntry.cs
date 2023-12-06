fd3ca5e0c446c1d508ef0afc9d02cad7b878fdda
Justin Grote <JustinGrote@users.noreply.github.com>
2023-12-06 09:48:21 -0800

    Cleanup Value naming

--- Source/PRTGPrd/SensorHistoryEntry.cs
+++ Source/PRTGPrd/SensorHistoryEntry.cs
@@ -20,12 +20,12 @@ public enum SensorValueType : Int16
 [DebuggerDisplay("DateTime = {DateTime}, SensorId = {SensorId}, ChannelId = {ChannelId}, Value = {Value}")]
 public record SensorHistoryEntry
 {
-  public DateTime DateTime { get; }
-  public Int32 SensorId { get; }
-  public Int32 ChannelId { get; }
-  public byte[] Unknown1 { get; }
-  public SensorValueType Type { get; }
-  byte[] _Value;
+  public DateTime DateTime { get; init; }
+  public Int32 SensorId { get; init; }
+  public Int32 ChannelId { get; init; }
+  public byte[] Unknown1 { get; init; }
+  public SensorValueType Type { get; init; }
+  byte[] value { get; init; }
   public double Value => Type switch
   {
     SensorValueType.Byte or
@@ -34,11 +34,11 @@ public record SensorHistoryEntry
     SensorValueType.Byte4 or
     SensorValueType.Neg1ChannelMaybe or
     SensorValueType.Neg2ChannelMaybe
-      => BitConverter.ToInt64(_Value, 0),
+      => BitConverter.ToInt64(value, 0),
 
-    SensorValueType.Double => BitConverter.ToDouble(_Value, 0),
+    SensorValueType.Double => BitConverter.ToDouble(value, 0),
 
-    _ => _Value[0] == 0 ? 0 : throw new NotImplementedException($"Don't know how to handle type '{Type}'.")
+    _ => value[0] == 0 ? 0 : throw new NotImplementedException($"Don't know how to handle type '{Type}'.")
   };
 
   internal SensorHistoryEntry(PrtgPrdReader reader)
@@ -48,6 +48,6 @@ public record SensorHistoryEntry
     ChannelId = reader.ReadInt32();
     Unknown1 = reader.ReadBytes(2);
     Type = (SensorValueType)reader.ReadInt16();
-    _Value = reader.ReadBytes(8);
+    value = reader.ReadBytes(8);
   }
 }
