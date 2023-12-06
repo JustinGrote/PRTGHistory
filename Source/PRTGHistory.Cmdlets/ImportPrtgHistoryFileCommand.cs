e01cc3fdfad6f217a31150868ef3364966393867
Justin Grote <JustinGrote@users.noreply.github.com>
2023-12-05 22:03:38 -0800

    ✨ Add Cmdlets and PRTGPrdReader

---
+++ Source/PRTGHistory.Cmdlets/ImportPrtgHistoryFileCommand.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Management.Automation;
+using PrtgDB;
+
+namespace PRTGPrd.Commands;
+
+[Cmdlet(VerbsData.Import, "PrtgHistoryFile")]
+public class ImportPrtgHistoryFileCommand : PSCmdlet
+{
+  [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true)]
+  [NotNull()]
+  public string Path { get; set; } = null!;
+
+  protected override void ProcessRecord()
+  {
+    try
+    {
+      WriteObject(new SensorHistoryFile(Path));
+    }
+    catch
+    {
+      WriteError(new ErrorRecord(new FileNotFoundException(), "FileNotFound", ErrorCategory.ObjectNotFound, Path));
+    }
+  }
+}
--- Source/PRTGPrd/SensorHistoryFile.cs
+++ Source/PRTGPrd/SensorHistoryFile.cs
@@ -1,39 +1,17 @@
-﻿using System.Collections;
-using static PrtgDB.SensorHistoryHeader;
-using System.IO.Pipelines;
+﻿using System.IO.Pipelines;
 using System.Text;
 
 namespace PrtgDB;
 
-public record SensorHistoryHeader(
-  long Version,
-  DateTime FirstDate,
-  DateTime LastDate,
-  int TotalRecords,
-  byte[] Unknown
-)
-{
-  public static SensorHistoryHeader ReadHeader(BinaryReader reader)
-  {
-    try
-    {
-      return new SensorHistoryHeader(
-        reader.ReadInt64(),
-        reader.ReadDate(),
-        reader.ReadDate(),
-        reader.ReadInt32(),
-        reader.ReadBytes(20)
-      );
-    }
-    catch (Exception ex)
-    {
-      throw new NotImplementedException($"Exception occurred while parsing file header: {ex.Message}", ex);
-    }
-  }
-}
-
+/// <summary>
+/// Represents a PRTG device sensor history file.
+/// </summary>
 public record SensorHistoryFile : IDisposable
 {
+  /// <summary>
+  /// The device ID is inferred from the filename. If this is -1, the filename was not in the expected format.
+  /// </summary>
+  public int DeviceId { get; init; }
   public string Path { get; init; }
   public SensorHistoryHeader Header { get; init; }
   public IEnumerable<SensorHistoryEntry> Entries { get; init; }
@@ -42,11 +20,18 @@ public record SensorHistoryFile : IDisposable
   public SensorHistoryFile(string path)
   {
     Path = path;
+
     stream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
 
-    using (BinaryReader reader = new(stream, Encoding.UTF8, leaveOpen: true))
+    string fileName = System.IO.Path.GetFileNameWithoutExtension(path);
+
+    DeviceId = int.TryParse(fileName.Substring(fileName.LastIndexOf(' ') + 1), out int deviceId)
+      ? deviceId
+      : -1;
+
+    using (PrtgPrdReader reader = new(stream, true))
     {
-      Header = ReadHeader(reader);
+      Header = reader.ReadHeader();
     }
 
     // This assumes we are at the point of the stream where the entries begin
@@ -58,22 +43,18 @@ public record SensorHistoryFile : IDisposable
     // Only one enumerator per stream at a time allowed to avoid seeks getting mixed up
     lock (content)
     {
-      long startPosition = content.Position;
-
       // Instantiate Pipelines for buffer management.
       Stream stream = PipeReader.Create(content).AsStream();
 
       // Read the stream into a buffer
-      using (BinaryReader reader = new(content, Encoding.UTF8, leaveOpen: true))
+
+      using (PrtgPrdReader reader = new(stream))
       {
         while (content.Position < content.Length)
         {
           yield return new SensorHistoryEntry(reader);
         }
       }
-
-      // Rewind the stream for subsequent requests (however there shouldn't be any because this should be fronted by a a cache)
-      content.Position = startPosition;
     }
   }
 
@@ -84,152 +65,45 @@ public record SensorHistoryFile : IDisposable
 }
 
 
-public static class BinaryReaderExtensions
+#pragma warning disable CS9107 // BinaryReader is fairly stateless so this is OK
+class PrtgPrdReader(Stream stream, bool leaveOpen = false) : BinaryReader(stream, Encoding.UTF8, leaveOpen)
+#pragma warning restore CS9107
 {
-  public static DateTime ReadDate(this BinaryReader reader)
+  public SensorHistoryHeader ReadHeader()
   {
-    var decimalValue = reader.ReadDouble();
+    if (stream.Position != 0)
+      throw new InvalidOperationException("Cannot read header from a stream that is not at the beginning");
+
+    try
+    {
+      return new SensorHistoryHeader(
+        ReadInt64(),
+        ReadDate(),
+        ReadDate(),
+        ReadInt32(),
+        ReadBytes(20)
+      );
+    }
+    catch (Exception ex)
+    {
+      throw new InvalidDataException($"Exception occurred while parsing file header: {ex.Message}", ex);
+    }
+  }
+  public DateTime ReadDate()
+  {
+    var decimalValue = ReadDouble();
     var dateValue = DateTime.FromOADate(decimalValue).ToLocalTime();
     return dateValue;
   }
+
+
 }
-//     private void Init(BinaryReader reader)
-//     {
-//       Version = reader.ReadInt64();
-//       FirstDate = ReadDate(reader);
-//       LastDate = ReadDate(reader);
-//       TotalRecords = reader.ReadInt32();
-//       Unknown = reader.ReadBytes(20);
 
-//       var list = new List<SensorHistoryFileChannel>();
+public record SensorHistoryHeader(
+  long Version,
+  DateTime FirstDate,
+  DateTime LastDate,
+  int TotalRecords,
+  byte[] Unknown
+);
 
-//       for (var i = 0; i < TotalRecords; i++)
-//       {
-//         try
-//         {
-//           list.Add(new SensorHistoryFileChannel(reader));
-//         }
-//         catch (Exception ex)
-//         {
-//           throw new NotImplementedException($"Exception occurred while parsing file '{Path}': {ex.Message}", ex);
-//         }
-//       }
-
-//   //todo: debuggerdisplay. update original commit
-//   public record SensorHistoryFile
-//   {
-//     public string Path { get; }
-
-//     public long Version { get; private set; }
-
-//     public DateTime FirstDate { get; private set; }
-
-//     public DateTime LastDate { get; private set; }
-
-//     public int TotalRecords { get; private set; }
-
-//     public byte[] Unknown { get; private set; }
-
-//     public ReadOnlyCollection<SensorHistoryFileChannel> Channels { get; set; }
-
-//     #region Deserialize
-
-//     public SensorHistoryFile(string path)
-//     {
-//       Path = path;
-
-//       using (var file = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-//       using (var reader = new BinaryReader(file))
-//       {
-//         Init(reader);
-//       }
-//     }
-
-//     internal SensorHistoryFile(BinaryReader reader)
-//     {
-//       Init(reader);
-//     }
-
-//     private void Init(BinaryReader reader)
-//     {
-//       Version = reader.ReadInt64();
-//       FirstDate = ReadDate(reader);
-//       LastDate = ReadDate(reader);
-//       TotalRecords = reader.ReadInt32();
-//       Unknown = reader.ReadBytes(20);
-
-//       var list = new List<SensorHistoryFileChannel>();
-
-//       for (var i = 0; i < TotalRecords; i++)
-//       {
-//         try
-//         {
-//           list.Add(new SensorHistoryFileChannel(reader));
-//         }
-//         catch (Exception ex)
-//         {
-//           throw new NotImplementedException($"Exception occurred while parsing file '{Path}': {ex.Message}", ex);
-//         }
-//       }
-
-//       Channels = new ReadOnlyCollection<SensorHistoryFileChannel>(list);
-//     }
-
-
-
-//     #endregion
-//     #region Serialize
-
-//     public SensorHistoryFile(
-//         string path,
-//         SensorHistoryFile example,
-//         IEnumerable<SensorHistoryFileChannel> channels,
-//         DateTime? firstDate = null,
-//         DateTime? lastDate = null)
-//     {
-//       if (path == null)
-//         throw new ArgumentNullException(nameof(path));
-
-//       if (example == null)
-//         throw new ArgumentNullException(nameof(example));
-
-//       if (channels == null)
-//         throw new ArgumentNullException(nameof(channels));
-
-//       Path = path;
-//       Version = example.Version;
-//       Unknown = example.Unknown;
-
-//       Channels = new ReadOnlyCollection<SensorHistoryFileChannel>(channels.ToList());
-
-//       if (Channels.Count == 0)
-//         throw new ArgumentException("At least one channel must be specified.", nameof(channels));
-
-//       var sorted = Channels.OrderBy(c => c.DateTime);
-
-//       FirstDate = firstDate ?? sorted.First().DateTime;
-//       LastDate = lastDate ?? sorted.Last().DateTime;
-//       TotalRecords = Channels.Count;
-//     }
-
-//     public byte[] Serialize()
-//     {
-//       using (var stream = new MemoryStream())
-//       using (var writer = new BinaryWriter(stream))
-//       {
-//         writer.Write(Version);
-//         writer.Write(FirstDate.ToUniversalTime().ToOADate());
-//         writer.Write(LastDate.ToUniversalTime().ToOADate());
-//         writer.Write(TotalRecords);
-//         writer.Write(Unknown);
-
-//         foreach (var channel in Channels)
-//           channel.Serialize(writer);
-
-//         return stream.ToArray();
-//       }
-//     }
-
-//     #endregion
-//   }
-// }
